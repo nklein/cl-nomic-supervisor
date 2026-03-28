9c564aa83447b7b082fe086fdeb14dc45b602cac
Patrick Stein <pat@nklein.com>
2026-03-28 11:17:59 -0500

    Cleaned up github api functions a bit

--- cl-nomic-supervisor.asd
+++ cl-nomic-supervisor.asd
@@ -9,7 +9,7 @@
   :author "Patrick Stein <pat@nklein.com>"
   :license "UNLICENSE"
   :version "0.1.20260322"
-  :depends-on (:alexandria :sqlite :ironclad :yason :dexador :toot)
+  :depends-on (:alexandria :quri :local-time :sqlite :ironclad :yason :dexador :toot)
   :components
   ((:static-file "README.md")
    (:static-file "UNLICENSE.txt")
@@ -19,6 +19,8 @@
                  (:file "git-cli" :depends-on ("package"
                                                "cli"))
                  (:file "http-constants" :depends-on ("package"))
+                 (:file "http-utils" :depends-on ("package"))
                  (:file "github-api" :depends-on ("package"
-                                                  "http-constants"))
+                                                  "http-constants"
+                                                  "http-utils"))
                  (:file "start" :depends-on ("package"))))))
--- src/github-api.lisp
+++ src/github-api.lisp
@@ -1,6 +1,6 @@
 (in-package :cl-nomic-supervisor)
 
-(defvar *github-api-url* "https://api.github.com")
+(defvar *github-api-host* "api.github.com")
 (defvar *github-api-version* "2026-03-10")
 (defvar *github-http-version* 1.1)
 
@@ -13,24 +13,26 @@
         *github-repo-owner* (uiop:getenv "GITHUB_REPO_OWNER")
         *github-repo-name* (uiop:getenv "GITHUB_REPO_NAME")))
 
-(defun attr (name json-object)
-  (cdr (assoc name json-object
-              :test #'string=)))
-
-(defun %github-api (method path
+(defun %github-api (method path-as-list
                     &key
                       (token *github-api-token*)
+                      query
                       body
-                      (api-url *github-api-url*)
+                      (api-host *github-api-host*)
                       (api-version *github-api-version*)
-                      (http-version *github-http-version*))
+                      (http-version *github-http-version*)
+                    &allow-other-keys)
+  (check-type method string)
+  (check-type path-as-list list)
   (check-type token string)
   (check-type body list)
-  (check-type api-url string)
+  (check-type api-host string)
   (check-type api-version string)
   (check-type http-version float)
   (multiple-value-bind (response-body response-status)
-      (dex:request (format nil "~A~A" api-url path)
+      (dex:request (quri:make-uri-https :host api-host
+                                        :path (%convert-path-as-list-to-encoded-path path-as-list)
+                                        :query (quri:url-encode-params query))
                    :method method
                    :version http-version
                    :headers `(("Authorization" . ,(format nil "Bearer ~A" token))
@@ -44,42 +46,36 @@
               (yason:parse response-body))
             response-status)))
 
-(defun list-pull-requests (&key
-                             (owner *github-repo-owner*)
-                             (repo *github-repo-name*))
-  (check-type owner string)
-  (check-type repo string)
-  (%github-api +GET+ (format nil "/repos/~A/~A/pulls"
-                             owner
-                             repo)))
+(defmacro define-github-api (name (method (&rest required-args)
+                                   &rest parameters)
+                             (&rest path-list) &body body)
+  (let ((rest (gensym "REST-")))
+    `(defun ,name (,@required-args
+                   &rest
+                     ,rest
+                   &key
+                     ,@parameters
+                   &allow-other-keys)
+       (apply #'%github-api ,method (list ,@path-list)
+              ,@body
+              ,rest))))
 
-(defun list-pull-request-reviews (pull-number
-                                  &key
-                                    (owner *github-repo-owner*)
-                                    (repo *github-repo-name*))
-  (check-type pull-number (integer 1 *))
-  (check-type owner string)
-  (check-type repo string)
-  (%github-api +GET+ (format nil "/repos/~A/~A/pulls/~A/reviews"
-                             owner
-                             repo
-                             pull-number)))
+(define-github-api list-pull-requests (+GET+ ()
+                                             (state "open")
+                                             (owner *github-repo-owner*)
+                                             (repo *github-repo-name*))
+    ("repos" owner repo "pulls")
+  :query `(("state" . ,state)))
 
-(defmacro string-case (keyform &body clauses)
-  (let ((key (gensym "KEY-")))
-    (labels ((compare (value)
-               `(string= ,value ,key))
-             (expand-clause (clause)
-               (destructuring-bind (value &rest body) clause
-                 `(,(etypecase value
-                      (string (compare value))
-                      (list `(or ,@(mapcar #'compare value)))
-                      ((member otherwise t) value))
-                   ,@body)
-)))
-      `(let ((,key ,keyform))
-         (cond
-           ,@(mapcar #'expand-clause clauses))))))
+(define-github-api list-pull-request-reviews (+GET+ (pull-number)
+                                                    (owner *github-repo-owner*)
+                                                    (repo *github-repo-name*))
+    ("repos" owner repo "pulls" pull-number "reviews"))
+
+(define-github-api list-pull-request-comments (+GET+ (pull-number)
+                                                     (owner *github-repo-owner*)
+                                                     (repo *github-repo-name*))
+    ("repos" owner repo "issues" pull-number "comments"))
 
 #+(or)
 (setf *github-api-token* "API-TOKEN-HERE"
@@ -87,25 +83,37 @@
       *github-repo-name* "cl-nomic-game-test")
 
 #+(or)
-(list-pull-requests)
+(list-pull-requests :state "all")
 
 #+(or)
-(let ((votes (make-hash-table :test 'equalp))
-      (all-comments))
-  (dolist (pull (list-pull-requests) (list (list* :votes
-                                                  (alexandria:hash-table-alist votes))
-                                           (list* :all-comments all-comments)))
-    (let ((state (attr "state" pull))
+(flet ((get-updated-at-timestamp (pr)
+         (local-time:parse-rfc3339-timestring (attr "updated_at" pr))))
+  (stable-sort (list-pull-requests :state "all")
+               #'local-time:timestamp<
+               :key #'get-updated-at-timestamp))
+
+#+(or)
+(list-pull-request-reviews 1)
+
+#+(or)
+(list-pull-request-comments 2)
+
+#+(or)
+(let (results)
+  (dolist (pull (list-pull-requests :state "all") results)
+    (let ((votes)
+          (all-comments)
+          (state (attr "state" pull))
           (number (attr "number" pull)))
-      (string-case state
-        ("open"
-         (dolist (review (list-pull-request-reviews number))
-           (let ((state (attr "state" review))
-                 (user (attr "login" (attr "user" review))))
-             (string-case state
-               ("COMMENTED"
-                (let ((body (attr "body" review)))
-                  (push (cons user body) all-comments)
-                  (string-case body
-                    (("APPROVED" "REJECTED")
-                     (setf (gethash user votes) body)))))))))))))
+      (dolist (comment
+               (list-pull-request-comments number)
+               (push (list number
+                           state
+                           (list* :votes (nreverse votes))
+                           (list* :comments (nreverse all-comments)))
+                     results))
+        (let ((user (attr "login" (attr "user" comment)))
+              (body (attr "body" comment)))
+          (push (cons user body) all-comments)
+          (when (member body '("APPROVED" "REJECTED") :test #'string=)
+            (push (cons user body) votes)))))))
