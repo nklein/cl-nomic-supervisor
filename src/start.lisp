1569e9303a407bf6652e3e5e74ad06d4e4280e55
Patrick Stein <pat@nklein.com>
2026-03-31 15:29:29 -0500

    Cleaned up a bit of the chaining, but maybe should rework cli-chain

--- src/start.lisp
+++ src/start.lisp
@@ -14,41 +14,46 @@
          (when ,output
            (princ ,output *standard-output*)
            (fresh-line *standard-output*)))
-       (zerop ,code))))
+       (unless (zerop ,code)
+         (error 'simple-error
+                :format-control "Exit code ~A for ~S"
+                :format-arguments (list ,code ',cmd)))
+       t)))
 
 (defun invoke-game (list-of-augmented)
   (let ((encoded (with-output-to-string (*standard-output*)
                    (json-encode list-of-augmented *standard-output*))))
     (with-input-from-string (*standard-input* encoded)
-      (relay-output ()
-        (uiop:run-program (list #P"/usr/bin/bwrap"
-                                "--ro-bind" "/bin" "/bin"
-                                "--ro-bind" "/lib" "/lib"
-                                "--ro-bind" "/usr/bin" "/usr/bin"
-                                "--ro-bind" "/usr/local" "/usr/local"
-                                "--bind" "/game" "/game"
-                                "--unshare-all"
-                                "--unshare-user"
-                                "--uid" "1001"
-                                "--gid" "1001"
-                                "--hostname" "nomic-game"
-                                "--chdir" "/game"
-                                "--unsetenv"
-                                "--new-session"
-                                "/bin/sh" "/game/start.sh")
-                          :input *standard-input*
-                          :output 'cl:string
-                          :error-output 'cl:string
-                          :ignore-error-status t
-                          :force-shell nil)))))
+      (uiop:run-program (list #P"/usr/bin/bwrap"
+                              "--ro-bind" "/bin" "/bin"
+                              "--ro-bind" "/lib" "/lib"
+                              "--ro-bind" "/usr/bin" "/usr/bin"
+                              "--ro-bind" "/usr/local" "/usr/local"
+                              "--bind" "/game" "/game"
+                              "--unshare-all"
+                              "--unshare-user"
+                              "--uid" "1001"
+                              "--gid" "1001"
+                              "--hostname" "nomic-game"
+                              "--chdir" "/game"
+                              "--unsetenv"
+                              "--new-session"
+                              "/bin/sh" "/game/start.sh")
+                        :input *standard-input*
+                        :output 'cl:string
+                        :error-output 'cl:string
+                        :ignore-error-status t
+                        :force-shell nil))))
 
 (defun %start ()
   (let ((list-of-augmented (or (with-open-file (*standard-input* #P"/tmp/sample.json")
                                  (json-parse *standard-input*))
                                (get-all-augmented-pull-requests))))
     (when list-of-augmented
-      (and (git-clone-repo-branch)
-           (invoke-game list-of-augmented)))))
+      (and (relay-output ()
+             (git-clone-repo-branch))
+           (relay-output ()
+             (invoke-game list-of-augmented))))))
 
 (defun start ()
   (handler-case
@@ -63,4 +68,4 @@
     (error (err)
       (let ((*print-escape* nil))
         (format *error-output* "ERROR: ~A~%" err)
-        #+sbcl (sb-ext:exit :code 1 :abort t)))))
+        #+sbcl (sb-ext:exit :code 2 :abort t)))))
