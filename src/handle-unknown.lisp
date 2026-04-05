22e2b7891c8a0ea35c7855ce464be26bcd56017a
Patrick Stein <pat@nklein.com>
2026-04-04 22:27:35 -0500

    Switch rollback logic to avoid looping

--- src/handle-unknown.lisp
+++ src/handle-unknown.lisp
@@ -11,23 +11,21 @@
   (format nil "revert-~A" (subseq original-sha 0 8)))
 
 (defun rollback (response)
-  (let* ((main-branch (get-branch-reference))
-         (original-sha {main-branch object sha})
+  (let* ((original-sha {(get-branch-reference) object sha})
          (tag-name (make-revert-name original-sha))
-         (tag (create-tag-reference tag-name original-sha))
          (main-commit (get-commit original-sha))
          (main-commit-parent-sha {(first {main-commit parents}) sha})
-         (parent-commit (get-commit main-commit-parent-sha))
-         (new-commit (create-commit (format nil "Reverting: ~A (~A)~%~%~A~%----~%RESPONSE:~%~A~%~%----~%~A~%"
-                                            original-sha
-                                            tag-name
-                                            {main-commit message}
-                                            response
-                                            "Reverted by CL-NOMIC-SUPERVISOR")
-                                    {parent-commit sha}
-                                    {parent-commit tree sha})))
-    (declare (ignore tag))
-    (update-branch-reference *git-default-branch* {new-commit sha})))
+         (tag-commit (create-commit-tag tag-name
+                                        original-sha
+                                        :message (format nil "Reverting: ~A (~A)~%~%~A~%----~%RESPONSE:~%~A~%~%----~%~A~%"
+                                                         original-sha
+                                                         tag-name
+                                                         {main-commit message}
+                                                         response
+                                                         "Reverted by CL-NOMIC-SUPERVISOR"))))
+    (create-tag-reference tag-name {tag-commit sha})
+    (update-branch-reference *git-default-branch*
+                             {(get-commit main-commit-parent-sha) sha})))
 
 (defun handle-unknown (response)
   (format *error-output* "Unknown response: ~A~%Rolling back~%" response)
